42e3c6271ba476443373a4a782f2579801cf255a
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2021-05-20 08:40:21 +0700

    add project

---
+++ notes-app-back-end/src/routes.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+
+const routes = [
+  {method: 'POST',
+    path: '/notes',
+    handler: () => {},
+    },
+  ];
+// eslint-disable-next-line eol-last
+module.exports = routes;
