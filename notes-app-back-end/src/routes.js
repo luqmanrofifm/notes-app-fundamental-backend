fd525be724f6e991f0d8ceeb7f2dd0b5f79757d0
luqmanrofifm <luqmanrmrofif@yahoo.co.id>
2021-05-20 09:24:31 +0700

    add save note

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,10 +1,12 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
 
+const { addNoteHandler } = require("./handler");
+
 const routes = [
   {method: 'POST',
     path: '/notes',
-    handler: () => {},
+    handler: addNoteHandler,
     },
   ];
 // eslint-disable-next-line eol-last
