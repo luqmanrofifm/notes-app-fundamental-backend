2eb4cf235cfb602e0ebc29dee6c8c73722b585fc
LuqmanRM <luqmanrofif@gmail.com>
2021-09-27 10:56:16 +0700

    change

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,23 +1,50 @@
-/* eslint-disable linebreak-style */
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+
+// notes
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+
+// users
+
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
       },
     },
   });
-  
-  server.route(routes);
 
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
 
 init();
